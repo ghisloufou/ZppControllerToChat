a8ddc783bc696340e5041538a1574336f058d4a3
rh0r <rhor.dev@gmail.com>
2022-09-09 21:12:28 +0200

    Premier commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ZppControllerToChat
-Petit programme pour transformer des inputs de manette XInput en texte dans le chat pour le zeventplays Pokémon
+Petit programme pour transformer des inputs de manette compatible XInput en texte dans le chat pour le [zeventplays Pokémon](https://www.twitch.tv/zeventplays).
+
+Faites des dons pour la planète ! Go vous renseigner sur https://zevent.fr !
+
+# Il est moche ton code
+Ch'ui un dev C# et c'est littéralement mon premier programme en python, donc c'est normal si vous voyez des trucs pas ouf :-)
+
+# Comment l'exécuter ?
+Il faut avoir python sur ton PC et exécuter le fichier main.py comme ça:
+
+    py .\main.py
+
+Je regarde pour en faire un truc facile à exécuter.
---
+++ main.py
@@ -0,0 +1,78 @@
+import XInput
+from pynput.keyboard import Key, Controller
+import time
+
+class MyHandler(XInput.EventHandler):
+    def __init__(self, *controllers, filter=...):
+        super().__init__(*controllers, filter=filter)
+        self.upper = False
+        self.stop = False
+        self.keyboard = Controller()
+        self.last_keystroke = time.time()
+
+    def process_button_event(self, event):
+        if event.type == XInput.EVENT_BUTTON_PRESSED:
+            match event.button_id:
+                case XInput.BUTTON_DPAD_UP:
+                    self.type_word("haut")
+                case XInput.BUTTON_DPAD_DOWN:
+                    self.type_word("bas")
+                case XInput.BUTTON_DPAD_LEFT:
+                    self.type_word("gauche")
+                case XInput.BUTTON_DPAD_RIGHT:
+                    self.type_word("droite")
+                case XInput.BUTTON_A:
+                    self.type_word("a")
+                case XInput.BUTTON_B:
+                    self.type_word("b")
+                case XInput.BUTTON_START:
+                    self.type_word("start")
+                case XInput.BUTTON_LEFT_THUMB:
+                    self.stop = True
+                case _:
+                    print(event)
+
+    def process_trigger_event(self, event):
+        return
+
+    def process_stick_event(self, event):
+        return
+
+    def process_connection_event(self, event):
+        return
+
+    def type_word(self, word):
+        now = time.time()
+
+        if (now - self.last_keystroke) > 1.5:
+            self.last_keystroke = now
+
+            output = ""
+            if self.upper:
+                output = word.upper()
+            else:
+                output = word
+
+            for c in output:
+                self.press_key(c)
+
+            self.press_key(Key.enter)
+
+            self.upper = not self.upper
+
+    def press_key(self, character):
+        self.keyboard.press(character)
+        self.keyboard.release(character)
+
+filter = XInput.BUTTON_DPAD_UP + XInput.BUTTON_DPAD_DOWN + XInput.BUTTON_DPAD_RIGHT + XInput.BUTTON_DPAD_LEFT + XInput.BUTTON_A + XInput.BUTTON_B + XInput.BUTTON_START + XInput.BUTTON_LEFT_THUMB
+my_handler = MyHandler(0)
+my_handler.set_filter(filter)
+
+print("Lezgo")
+
+my_gamepad_thread = XInput.GamepadThread(my_handler)
+
+while(not my_handler.stop):
+    continue
+
+print("Tchao")
